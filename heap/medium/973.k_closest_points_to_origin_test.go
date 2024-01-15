ffd856deea361544e15e6418510dbc845678368b
JamesHooMY <hoosinyong@hotmail.com>
2024-01-15 16:09:38 +0800

    feat: add max heap method to 973. K Closest Points to Origin

--- heap/medium/973.k_closest_points_to_origin_test.go
+++ heap/medium/973.k_closest_points_to_origin_test.go
@@ -41,7 +41,6 @@ func (h *MinHeapPoint) Pop() any {
 // 3) init the min heap slice to a min heap using heap.Init()
 // 4) pop k points from the min heap
 // TC: O(NlogN), SC: O(N), N is the length of points
-// * this is the best solution for me currently
 func kClosest1(points [][]int, k int) [][]int {
 	// every time heap push will compare with the parent node, it slower than code below
 	// h := &MinHeapPoint{}
@@ -67,6 +66,54 @@ func kClosest1(points [][]int, k int) [][]int {
 
 	var result [][]int
 	for i := 0; i < k; i++ {
+		point := heap.Pop(h).(Point)
+		result = append(result, []int{point.x, point.y})
+	}
+
+	return result
+}
+
+type MaxHeapPoint []Point
+
+func (h *MaxHeapPoint) Len() int { return len(*h) }
+
+func (h *MaxHeapPoint) Less(i, j int) bool { return (*h)[i].distance > (*h)[j].distance }
+
+func (h *MaxHeapPoint) Swap(i, j int) { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *MaxHeapPoint) Push(x any) { *h = append(*h, x.(Point)) }
+
+func (h *MaxHeapPoint) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+// method 2 max heap
+// 1) create a max heap slice
+// 2) push all points to the heap slice
+// 3) pop k points from the max heap
+// TC: O(NlogK), SC: O(K), N is the length of points
+// * this is the best solution for me currently
+func kClosest2(points [][]int, k int) [][]int {
+	h := &MaxHeapPoint{}
+	for _, point := range points {
+		heap.Push(h, Point{
+			x:        point[0],
+			y:        point[1],
+			distance: point[0]*point[0] + point[1]*point[1],
+		})
+
+		// keep the size of heap is k
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+
+	var result [][]int
+	for h.Len() > 0 {
 		point := heap.Pop(h).(Point)
 		result = append(result, []int{point.x, point.y})
 	}
@@ -124,10 +171,69 @@ func Test_kClosest1(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		assert.Equal(
+		assert.ElementsMatch(
 			t,
 			tc.expected.result,
 			kClosest1(tc.args.points, tc.args.k),
+			fmt.Sprintf("testCase name: %s", tc.name),
+		)
+	}
+}
+
+func Test_kClosest2(t *testing.T) {
+	type args struct {
+		points [][]int
+		k      int
+	}
+	type expected struct {
+		result [][]int
+	}
+	type testCase struct {
+		name     string
+		args     args
+		expected expected
+	}
+
+	testCases := []testCase{
+		{
+			name: "1",
+			args: args{
+				points: [][]int{
+					{1, 3},
+					{-2, 2},
+				},
+				k: 1,
+			},
+			expected: expected{
+				result: [][]int{
+					{-2, 2},
+				},
+			},
+		},
+		{
+			name: "2",
+			args: args{
+				points: [][]int{
+					{3, 3},
+					{5, -1},
+					{-2, 4},
+				},
+				k: 2,
+			},
+			expected: expected{
+				result: [][]int{
+					{3, 3},
+					{-2, 4},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		assert.ElementsMatch(
+			t,
+			tc.expected.result,
+			kClosest2(tc.args.points, tc.args.k),
 			fmt.Sprintf("testCase name: %s", tc.name),
 		)
 	}
@@ -137,6 +243,15 @@ func Test_kClosest1(t *testing.T) {
 func Benchmark_kClosest1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		kClosest1([][]int{
+			{1, 3},
+			{-2, 2},
+		}, 1)
+	}
+}
+
+func Benchmark_kClosest2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		kClosest2([][]int{
 			{1, 3},
 			{-2, 2},
 		}, 1)
